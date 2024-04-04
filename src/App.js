4834f0f56ab507567a9fb4973fb698dd40f88d47
developer5001 <abdujamoiddinovl2009@gmail.com>
2024-04-04 17:18:17 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Account from "./pages/Account/Account";
+import Announcement from "./pages/Announcement/Announcement";
+import Chat from "./pages/Chat/Chat";
+import { useInfoContext } from "./context/infoContext";
 
 function App() {
+  const { currentUser } = useInfoContext();
   return (
-    <div className="App">
-        <h1>Hello App</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/announcement" element={<Announcement />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
   );
 }
 
